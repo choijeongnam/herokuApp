2fee9d3cade1e826a173ac01d523639d647fe79d
Choi Jeong Nam <62236509+choijeongnam@users.noreply.github.com>
2022-05-24 17:29:44 +0900

    first commit

---
+++ src/test/java/com/dks/heroku/HerokuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dks.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
