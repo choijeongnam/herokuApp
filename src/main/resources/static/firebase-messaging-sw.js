655914460fd77117349602c33d28716a707d2fca
Choi Jeong Nam <62236509+choijeongnam@users.noreply.github.com>
2022-05-27 10:41:19 +0900

    프로젝트 키

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -1,23 +1,23 @@
 // Give the service worker access to Firebase Messaging.
-  // Note that you can only use Firebase Messaging here. Other Firebase libraries
-  // are not available in the service worker.
-  importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
-  importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-  // Initialize the Firebase app in the service worker by passing in
-  // your app's Firebase config object.
-  // https://firebase.google.com/docs/web/setup#config-object
-  firebase.initializeApp({
-    apiKey: 'api-key',
-    authDomain: 'project-id.firebaseapp.com',
-    databaseURL: 'https://project-id.firebaseio.com',
-    projectId: 'project-id',
-    storageBucket: 'project-id.appspot.com',
-    messagingSenderId: 'sender-id',
-    appId: 'app-id',
-    measurementId: 'G-measurement-id',
-  });
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+	apiKey: "AIzaSyCtHSTHw95PqQZCa1VawWG0NCRqUSOKTyA",
+	authDomain: "lottehotel-web-push.firebaseapp.com",
+	projectId: "lottehotel-web-push",
+	storageBucket: "lottehotel-web-push.appspot.com",
+	messagingSenderId: "44131432600",
+	appId: "1:44131432600:web:996617819428bf5d0478ed",
+	measurementId: "G-0SFKW8CVNM"
+});
 
-  // Retrieve an instance of Firebase Messaging so that it can handle background
-  // messages.
-  const messaging = firebase.messaging();
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
